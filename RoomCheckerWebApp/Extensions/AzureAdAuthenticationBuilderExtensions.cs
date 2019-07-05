7d9fc3365275054736b031455bde2b9dc6a6fc0d
Sander van den Hoven <sandervd@microsoft.com>
2019-07-05 09:27:30 +0200

    Reverted back to inmem token cache, due to unknown bug with azure table version

--- RoomCheckerWebApp/Extensions/AzureAdAuthenticationBuilderExtensions.cs
+++ RoomCheckerWebApp/Extensions/AzureAdAuthenticationBuilderExtensions.cs
@@ -87,7 +87,8 @@ namespace MicrosoftGraphAspNetCoreConnectSample.Extensions
                             _azureOptions.ClientId, 
                             _azureOptions.BaseUrl + _azureOptions.CallbackPath,
                             new ClientCredential(_azureOptions.ClientSecret),
-                            new AzureTableTokenCache(identifier,_azureOptions.TokenCacheConnectionString, _azureOptions.TokenCacheTableName).GetCacheInstance(), 
+                            new SessionTokenCache(identifier, memoryCache).GetCacheInstance(),
+                            //new AzureTableTokenCache(identifier,_azureOptions.TokenCacheConnectionString, _azureOptions.TokenCacheTableName).GetCacheInstance(), 
                             null);
                         var result = await cca.AcquireTokenByAuthorizationCodeAsync(code, graphScopes);
 
